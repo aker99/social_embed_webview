0235e49f6f0300342fa718548d8b30edcd7cc02c
keithchan <keith3chan@gmail.com>
2022-08-10 12:14:21 +0800

    Remove fixed aspect ratio, and pass in raw embed code as htmlBody

--- lib/platforms/facebook-video.dart
+++ lib/platforms/facebook-video.dart
@@ -1,21 +1,17 @@
-import 'package:flutter/material.dart';
-
 import 'social-media-generic.dart';
 
 class FacebookVideoEmbedData extends SocialMediaGenericEmbedData {
-  final String videoUrl;
+  final String embedHtml;
 
-  const FacebookVideoEmbedData({required this.videoUrl})
-      : super(aspectRatio: 16 / 9);
+  const FacebookVideoEmbedData({required this.embedHtml})
+      : super(canChangeSize: true, bottomMargin: -10);
 
   @override
   String get htmlScriptUrl =>
       'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2';
 
   @override
-  String get htmlBody =>
-      '<div id="fb-root"></div><div class="fb-video" data-href="$videoUrl"></div>' +
-      htmlScript;
+  String get htmlBody => embedHtml;
 
   @override
   String get pauseVideoScript => "pauseVideo()";
